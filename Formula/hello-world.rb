5acecdf1baa47135cc4525ba72d8f31f2fa21a01
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-10 21:54:27 +0000

    hello-world: add 9.0.0 bottle.

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -5,6 +5,12 @@ class HelloWorld < Formula
   url "https://github.com/ladislas/hello-world/archive/9.0.0.tar.gz"
   sha256 "5b4bb2c4d96e2d9adb38b4f92ab20a1806a6411f902c712ea9cf5c1a5a7ab65b"
 
+  bottle do
+    root_url "https://github.com/ladislas/homebrew-hello/releases/download/hello-world-9.0.0"
+    cellar :any_skip_relocation
+    sha256 "529725a528e0fc60fb58d0b974fa29d1b099214ef5de57720758a74b104a5576" => :catalina
+  end
+
   def install
     system "make"
     bin.install "build/hello"
