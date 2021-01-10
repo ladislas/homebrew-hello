3d9eca4f266bf997279e534372750937bc5b82bd
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-10 16:22:33 +0100

    Update to 9

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,9 +2,8 @@ class HelloWorld < Formula
   desc "Simple C++ Hello World CLI to play with Homebrew"
   homepage "https://github.com/ladislas/hello-world"
 
-  url "https://github.com/ladislas/hello-world/archive/8.0.0.tar.gz"
+  url "https://github.com/ladislas/hello-world/archive/9.0.0.tar.gz"
   sha256 "0caa1306c4a6c629aed2b66080375c24fc3b820fd803f9e327ae140a30da336d"
-  revision 3
 
   def install
     system "make"
