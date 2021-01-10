5d7e97ce59723cfb86bd1df400edda1afe0672c3
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-10 15:18:08 +0100

    hello-world 8.0.0 (new formula)

---
+++ Formula/hello-world.rb
@@ -0,0 +1,17 @@
+class HelloWorld < Formula
+  desc "Simple C++ Hello World CLI to play with Homebrew"
+  homepage "https://github.com/ladislas/hello-world"
+
+  url "https://github.com/ladislas/hello-world/archive/8.0.0.tar.gz"
+  sha256 "0caa1306c4a6c629aed2b66080375c24fc3b820fd803f9e327ae140a30da336d"
+  revision 3
+
+  def install
+    system "make"
+    bin.install "build/hello"
+  end
+
+  test do
+    assert_equal `hello`, "Hello, #{ENV["USER"]}!\nVersion #{version}\n"
+  end
+end
